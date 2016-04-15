4875e5c35af7fe036dfd4604ee49884cdb72bfce
unknown <corantin.noll@gmail.com>
2016-04-15 10:11:13 -0400

    Startup

---
+++ Code/GeoLoc/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+
+
+namespace GeoLoc
+{
+    // Ce pogramme est le lecteur de la MsgQueue
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageQueue messageQueue = new MessageQueue(@".\Private$\SomeTestName");
+            System.Messaging.Message[] messages = messageQueue.GetAllMessages();
+
+            foreach (System.Messaging.Message message in messages)
+            {
+
+                Console.WriteLine(message.Label);
+                Console.WriteLine(message.Body);
+                //Console.WriteLine(message.);
+            }
+            // after all processing, delete all the messages
+            messageQueue.Purge();
+
+        }
+    }
+}
---
+++ Code/LecteurEvent/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+
+namespace LecteurEvent
+{
+    // Ce programme est clui qui écrit dans la mg queue
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageQueue messageQueue = null;
+            if (MessageQueue.Exists(@".\Private$\SomeTestName"))
+            {
+                messageQueue = new MessageQueue(@".\Private$\SomeTestName");
+                messageQueue.Label = "Testing Queue";
+            }
+            else
+            {
+                // Create the Queue
+                MessageQueue.Create(@".\Private$\SomeTestName");
+                messageQueue = new MessageQueue(@".\Private$\SomeTestName");
+                messageQueue.Label = "Newly Created Queue";
+            }
+            messageQueue.Send("First ever Message is sent to MSMQ", "Message 1");
+            messageQueue.Send("First ever Message is sent to MSMQ", "Msg 2");
+            messageQueue.Send("First ever Message is sent to MSMQ", "Msg 3");
+            messageQueue.Send("First ever Message is sent to MSMQ", "Msg 4");
+            messageQueue.Send("First ever Message is sent to MSMQ", "Msg 5");
+        }
+    }
+}
